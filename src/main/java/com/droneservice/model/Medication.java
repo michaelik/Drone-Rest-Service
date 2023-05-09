7c14302186881c4acea2d76130d00aaa753126e9
michaelik <mikeikechi3@gmail.com>
2023-05-09 09:41:16 +0100

    refactor: eliminate unuse import on medication model

--- src/main/java/com/droneservice/model/Medication.java
+++ src/main/java/com/droneservice/model/Medication.java
@@ -1,7 +1,10 @@
 package com.droneservice.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import javax.persistence.Column;
